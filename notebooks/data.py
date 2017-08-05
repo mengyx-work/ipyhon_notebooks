2e52e1c64b594b6d5d92e4528ee06d6877e34a71
Matt Meng <matt.meng@matt-m-mbp.local>
2017-08-05 14:54:32 -0700

    update

--- notebooks/data.py
+++ notebooks/data.py
@@ -1,4 +1,22 @@
 import cPickle as pickle
+import numpy as np
+
+def batch(inputs, max_sequence_length=None):
+    sequence_lengths = [len(seq) for seq in inputs]
+    batch_size = len(inputs)
+    if max_sequence_length is None:
+        max_sequence_length = max(sequence_lengths)
+    inputs_batch_major = np.zeros(shape=[batch_size, max_sequence_length], dtype=np.int32) # == PAD
+
+    for i, seq in enumerate(inputs):
+        for j, element in enumerate(seq):
+            inputs_batch_major[i, j] = element
+
+    # [batch_size, max_time] -> [max_time, batch_size]
+    inputs_time_major = inputs_batch_major.swapaxes(0, 1)
+
+    return inputs_time_major, sequence_lengths
+
 
 class DataGenerator(object):
 
@@ -9,6 +27,7 @@ class DataGenerator(object):
         self.titles = self.data['titles']
         self.reverse_token_dict = self.data['reverse_token_dict']
         self.data_size = len(self.titles)
+        self.vocab_size = max(self.reverse_token_dict.keys())
     
     def generate_sequence(self, batch_size):
         if batch_size >= 2 * self.data_size:
