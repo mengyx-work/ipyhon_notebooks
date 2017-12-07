3b2a3f6addab90e86ecb16744ec1ae78ec5f7e8a
mengyx-work <ymeng.ucla@gmail.com>
2017-12-06 23:02:06 -0800

    add eval mode in training and also the model restore

--- speech/speech_model/tensorflow_model.py
+++ speech/speech_model/tensorflow_model.py
@@ -1,10 +1,12 @@
 import os, time
 import tensorflow as tf
+from utils import clear_folder, model_meta_file
+from create_tensorboard_start_script import generate_tensorboard_script
 
 
 class TensorFlowModel(object):
     def __init__(self, mode, log_path, model_path, model_settings,
-                 model_building_fn=None, saving_steps=20, dipslay_steps=10):
+                 model_building_fn=None, saving_steps=6000, dipslay_steps=4000):
         self.log_path = log_path
         self.model_path = model_path
         self.saving_steps = saving_steps
@@ -14,23 +16,40 @@ class TensorFlowModel(object):
         self.sess = tf.Session(graph=self.graph, config=self.model_settings['sess_config'])
         self.global_step = 0
 
-        MODEL_MODES = ['train', 'restore_model', 'eval']
+        MODEL_MODES = ['train', 'restore_train', 'eval']
         if mode not in MODEL_MODES:
             raise ValueError('failed to recognize given mode {}'.format(mode))
+
         if mode == 'train':
             if model_building_fn is None:
                 raise ValueError('the model building function is missing')
             self._build_training_graph(model_building_fn)
 
+        if mode == 'restore_train':
+            self._restore_model()
+
     def _build_training_graph(self, build_model):
+        clear_folder(self.log_path)
+        clear_folder(self.model_path)
+        generate_tensorboard_script(self.log_path)  # create the script to start a Tensorboard
+
         with self.graph.as_default():
             self._init_placeholders()
-            logits = build_model(self.wav_inputs, self.model_settings)
-            self.loss = self._build_loss(logits)
+            with tf.name_scope('model'):
+                self.logits = build_model(self.wav_inputs, self.model_settings, 'logit_output')
+            self.loss = self._build_loss(self.logits)
             self._build_optimizer()
+            self._build_predictor()
             self.saver = tf.train.Saver(max_to_keep=2, keep_checkpoint_every_n_hours=1)
             init = tf.global_variables_initializer()
             self.sess.run(init)
+
+    def _build_predictor(self):
+        with tf.name_scope('predictor'):
+            predict_index = tf.argmax(self.logits, axis=1, output_type=tf.int32)
+            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(predict_index, self.ground_truth_labels), tf.float32),
+                                           name='accuracy')
+        tf.summary.scalar('accuracy', self.accuracy)
 
     def _init_placeholders(self):
         with tf.name_scope('initial_inputs'):
@@ -45,34 +64,78 @@ class TensorFlowModel(object):
                                                       self.global_saving_steps + self.saving_steps,
                                                       name="increment_saving_step_op")
 
+    def _restore_model(self):
+        with self.graph.as_default():
+            self.saver = tf.train.import_meta_graph(model_meta_file(self.model_path))
+            self.saver.restore(self.sess, tf.train.latest_checkpoint(self.model_path))
+            self._restore_placeholders()
+            self._restore_variable()
+
+    def _restore_placeholders(self):
+        self.wav_inputs = self.sess.graph.get_tensor_by_name("initial_inputs/wav_inputs:0")
+        self.dropout_prob = self.sess.graph.get_tensor_by_name("initial_inputs/dropout_prob:0")
+        self.learning_rate_input = self.sess.graph.get_tensor_by_name("initial_inputs/learning_rate_input:0")
+        self.ground_truth_labels = self.sess.graph.get_tensor_by_name("initial_inputs/ground_truth_labels:0")
+        self.global_saving_steps = self.sess.graph.get_tensor_by_name("initial_inputs/global_saving_steps:0")
+        self.global_step = self.sess.run(self.global_saving_steps)
+
+    def _restore_variable(self):
+        self.logits = self.sess.graph.get_operation_by_name("model/logit_output")
+        self.train_op = self.sess.graph.get_operation_by_name("optimizer/train_op")
+        self.increment_saving_step_op = self.sess.graph.get_operation_by_name("initial_inputs/increment_saving_step_op")
+        self.loss = self.sess.graph.get_tensor_by_name("loss/reduce_mean_loss:0")
+        self.accuracy = self.sess.graph.get_operation_by_name("predictor/accuracy")
+
     def _build_loss(self, logits):
         with tf.name_scope('loss'):
             cross_entropy_mean = tf.reduce_mean(
                 tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.ground_truth_labels,
-                                                               logits=logits))
+                                                               logits=logits), name='reduce_mean_loss')
         tf.summary.scalar('cross_entropy', cross_entropy_mean)
         return cross_entropy_mean
 
     def _build_optimizer(self):
-        with tf.name_scope('train'):
-            self.train_step = tf.train.GradientDescentOptimizer(self.learning_rate_input).minimize(self.loss)
+        with tf.name_scope('optimizer'):
+            #self.train_op = tf.train.GradientDescentOptimizer(self.learning_rate_input).minimize(self.loss, name='train_op')
+            self.train_op = tf.train.AdamOptimizer(learning_rate=self.learning_rate_input).minimize(self.loss, name='train_op')
 
-    def _next_feed(self, batches, learning_rate):
+    def _run_predictor(self, test_batches):
+        feed_content = self._next_feed(test_batches, 'eval')
+        accuracy = self.sess.run(self.accuracy, feed_content)
+        return accuracy
+
+    def _next_feed(self, batches, mode='train', learning_rate=None):
+        if mode not in ['train', 'eval']:
+            raise ValueError('failed to recognize the mode {} in `_next_feed`'.format(mode))
         data_batch, target_batch = next(batches)
+
+        if mode == 'train':
+            return {self.wav_inputs: data_batch,
+                    self.ground_truth_labels: target_batch,
+                    self.dropout_prob: 0.5,
+                    self.learning_rate_input: learning_rate
+                    }
+
         return {self.wav_inputs: data_batch,
                 self.ground_truth_labels: target_batch,
-                self.dropout_prob: 0.5,
-                self.learning_rate_input: learning_rate
+                self.dropout_prob: 1.0,
                 }
 
     def _saving_step_run(self):
         _ = self.sess.run([self.increment_saving_step_op])
         self.saver.save(self.sess, os.path.join(self.model_path, 'models'), global_step=self.global_step)
 
-    def _display_step_run(self, start_time, feed_content):
+    def _display_step_run(self, start_time, feed_content, test_batches=None):
+        _start_time = time.time()
+        if test_batches is not None:
+            accuracy_ = self._run_predictor(test_batches)
+            print('using {:.2f} seconds, accuracy from test data is {}'.format((time.time() - _start_time), accuracy_))
+
         summary, loss_value = self.sess.run([self.merged_summary_op, self.loss], feed_content)
         self.writer.add_summary(summary, self.global_step)
-        print 'step {}, batch loss: {} and using {:.2f} seconds'.format(self.global_step, loss_value, time.time()-start_time)
+        print('step {}, batch loss: {} and using {:.2f} seconds'.format(self.global_step,
+                                                                        loss_value,
+                                                                        time.time() - start_time))
 
     def _reach_display_step(self):
         return self.global_step == 1 or self.global_step % self.display_steps == 0
@@ -80,21 +143,20 @@ class TensorFlowModel(object):
     def _reach_saving_step(self):
         return self.global_step % self.saving_steps == 0
 
-    def train(self, batch_generator, num_batches):
+    def train(self, batches, num_batches, test_batches=None):
         with self.graph.as_default():
             self.merged_summary_op = tf.summary.merge_all()
             self.writer = tf.summary.FileWriter(self.log_path, graph=self.graph)
-
             cur_time = time.time()
             while self.global_step < num_batches:
-                feed_content = self._next_feed(batch_generator, 0.0001)
-                _ = self.sess.run([self.train_step], feed_content)
+                feed_content = self._next_feed(batches, 'train', 0.0001)
+                _ = self.sess.run([self.train_op], feed_content)
                 self.global_step += 1
 
                 if self._reach_saving_step():
                     self._saving_step_run()
 
                 if self._reach_display_step():
-                    self._display_step_run(cur_time, feed_content)
+                    self._display_step_run(cur_time, feed_content, test_batches)
                     cur_time = time.time()
             self.saver.save(self.sess, os.path.join(self.model_path, 'complete_model'), global_step=self.global_step)
--- speech/speech_model/train_runner.py
+++ speech/speech_model/train_runner.py
@@ -4,27 +4,31 @@ from tensorflow_model import TensorFlowModel
 from data_generator import DataGenerator
 
 
-def build_nenural_network_model(wav_input, model_settings):
+def build_nenural_network_model(wav_input, model_settings, logits_name='output'):
     frame_size = model_settings['frame_size']
     frame_num = model_settings['frame_num']
-    #batch_size, frame_num_, frame_size_ = tf.unstack(tf.shape(wav_input))
     label_size = model_settings['label_size']
     hidden_layer_output_size = model_settings['hidden_layer_size']
 
     hidden_layer_weight = tf.Variable(tf.truncated_normal([frame_size * frame_num, hidden_layer_output_size], stddev=0.001),
                                       name='hidden_layer_weight')
     hidden_layer_bias = tf.Variable(tf.zeros([hidden_layer_output_size]), name='hidden_layer_bias')
+    tf.summary.histogram("model/hidden_layer_weight", hidden_layer_weight)
+
     flat_wav_input = tf.reshape(wav_input, (-1, frame_size * frame_num))
     hidden_layer_output = tf.add(tf.matmul(flat_wav_input, hidden_layer_weight), hidden_layer_bias)
 
     output_weights = tf.Variable(tf.truncated_normal([hidden_layer_output_size, label_size]), name='output_weights')
     output_bias = tf.Variable(tf.zeros([label_size]), name='output_bias')
-    logits = tf.add(tf.matmul(hidden_layer_output, output_weights), output_bias)
+    tf.summary.histogram("model/output_weights", output_weights)
+
+    logits = tf.add(tf.matmul(hidden_layer_output, output_weights), output_bias, name=logits_name)
     return logits
 
 
 COMMON_PATH = os.path.join(os.path.expanduser("~"), 'local_tensorflow_content')
 NUM_THREADS = 6
+
 
 def train_model(model_name='speech_neural_network_model', USE_GPU=False):
     model_path = os.path.join(COMMON_PATH, model_name)
@@ -32,12 +36,15 @@ def train_model(model_name='speech_neural_network_model', USE_GPU=False):
 
     data_path = '/Users/matt.meng/data/speech_competition/processed_data'
     data_generator = DataGenerator(data_path)
+    test_data_generator = DataGenerator(data_path)
+    test_batches = test_data_generator.generate_batch_iter(3000)
+
     batch_size = 256
     batches = data_generator.generate_batch_iter(batch_size)
     model_settings = {'frame_num': data_generator.frame_num,
                       'frame_size': data_generator.frame_size,
                       'label_size': 12,
-                      'hidden_layer_size': 256}
+                      'hidden_layer_size': 512}
     if USE_GPU:
         model_settings['sess_config'] = tf.ConfigProto(log_device_placement=False)
         os.environ['CUDA_VISIBLE_DEVICES'] = '1'
@@ -46,7 +53,8 @@ def train_model(model_name='speech_neural_network_model', USE_GPU=False):
         model_settings['sess_config'] = tf.ConfigProto(intra_op_parallelism_threads=NUM_THREADS)
 
     model = TensorFlowModel('train', log_path, model_path, model_settings, model_building_fn=build_nenural_network_model)
-    model.train(batches, 100000)
+    #model = TensorFlowModel('restore_train', log_path, model_path, model_settings, model_building_fn=build_nenural_network_model)
+    model.train(batches, 100000,test_batches)
 
 if __name__ == '__main__':
     train_model()
